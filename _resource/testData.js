f794822ab4678c9152420e3cc2c2568befa83aa0
stanislavrepin <41024762+stanislavrepin@users.noreply.github.com>
2021-08-19 21:26:34 -0700

    first commit

---
+++ _resource/testData.js
@@ -0,0 +1,16 @@
+export const testUser = {
+  userName: 'g424414',
+  email: 'g4244149520@gmail.com',
+  password: 'testPa$$W0rd',
+};
+
+export const expToken = 'ghp_NA9Pnbfgld9UwGCaAZeHh9GIQw1FSJ1p1cYn';
+
+export const newRepo = {
+  name: 'newRepo',
+  description: 'created using http req',
+  homepage: 'https://github.com',
+  private: false,
+  has_projects: true,
+  auto_init: true,
+};
--- _src/reposTest.spec.js
+++ _src/reposTest.spec.js
@@ -1,49 +1,88 @@
 import axiosInstance from '../default.config';
-import { testUser, expToken } from '../_resource/testData';
+import { testUser, expToken, newRepo } from '../_resource/testData';
 import { expect } from 'chai';
 
 describe('GitHub repos tests', async () => {
-  it('Should get public repositories list', async () => {
-    const response = await axiosInstance.get(`/users/${testUser.userName}/repos`);
-
-    expect(response.status).equal(200);
-    expect(response.data.length).equal(2);
-  });
-
-  it('Should get all repositories list', async () => {
-    const response = await axiosInstance.get('/user/repos', {
-      headers: {
-        Authorization: `Bearer ${process.env.API_TOKEN}`,
-        accept: 'application/vnd.github.v3+json',
-      },
-    });
-    expect(response.status).equal(200);
-    expect(response.data.length).equal(4);
-  });
-
-  it('Should not get all repositories list without token', async () => {
-    try {
-      await axiosInstance.get('/user/repos', {
-        headers: {
-          Authorization: '',
-          accept: 'application/vnd.github.v3+json',
-        },
+  context('Get repositories', async () => {
+    it('Should get public repositories list', async () => {
+      await axiosInstance.get(`/users/${testUser.userName}/repos`).then((res) => {
+        expect(res.status).equal(200);
+        expect(res.data.length).equal(2);
       });
-    } catch (err) {
-      expect(err.response.status).equal(401);
-    }
+    });
+
+    it('Should get all repositories list', async () => {
+      await axiosInstance
+        .get('/user/repos', {
+          headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`,
+            accept: 'application/vnd.github.v3+json',
+          },
+        })
+        .then((res) => {
+          expect(res.status).equal(200);
+          expect(res.data.length).equal(4);
+        });
+    });
+
+    it('Should not get all repositories list without token', async () => {
+      await axiosInstance
+        .get('/user/repos', {
+          headers: {
+            Authorization: '',
+            accept: 'application/vnd.github.v3+json',
+          },
+        })
+        .catch((err) => expect(err.response.status).equal(401));
+    });
   });
 
   it('Should not get all repositories list with expired token', async () => {
-    try {
-      await axiosInstance.get('/user/repos', {
+    await axiosInstance
+      .get('/user/repos', {
         headers: {
           Authorization: `Bearer ${expToken}`,
           accept: 'application/vnd.github.v3+json',
         },
-      });
-    } catch (err) {
-      expect(err.response.status).to.equal(401);
-    }
+      })
+      .catch((err) => expect(err.response.status).equal(401));
   });
 });
+
+context('Create and modify repository', async () => {
+  it('Should create new private repository for the authenticated user', async () => {
+    await axiosInstance
+      .post('/user/repos', newRepo, {
+        headers: {
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
+          accept: 'application/vnd.github.v3+json',
+        },
+      })
+      .then((res) => expect(res.status).equal(201));
+  });
+
+  it('Should not create new private repository if it exist', async () => {
+    await axiosInstance
+      .post('/user/repos', newRepo, {
+        headers: {
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
+          accept: 'application/vnd.github.v3+json',
+        },
+      })
+      .catch((err) => expect(err.response.status).equal(422));
+  });
+
+  it('Should delete repository', async () => {
+    await axiosInstance
+      .delete(`/repos/${testUser.userName}/${newRepo.name}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
+          accept: 'application/vnd.github.v3+json',
+        },
+      })
+      .then((res) => expect(res.status).equal(204));
+  });
+});
+
+// .then((res) => console.log(res))
+// .catch((err) => console.log(err));
